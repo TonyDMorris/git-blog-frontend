9bd1fef52a4ae6c7d572f63e61a87b82468f54cf
TonyDMorris <anthonymorris86@googlemail.com>
2024-01-18 21:04:48 +0000

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import Logo from "../Assets/image.png";
 
 function Footer() {
   let date = new Date();
@@ -15,16 +9,12 @@ function Footer() {
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
-            href="https://flowbite.com/"
+            href="https://diff.monster"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
+            <img src={Logo} className="h-8" alt="Diff Monster Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Diff Monster
             </span>
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -53,8 +43,8 @@ function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
+          <a href="https://diff.monster" className="hover:underline">
+            Diff Monster™
           </a>
           . All Rights Reserved.
         </span>
